d520dd6d3eafbb32aef782413316cd16125bc25f
EM0523 <145616364+EM0523@users.noreply.github.com>
2023-10-02 22:35:41 -0700

    Add files via upload

---
+++ crabs.m
@@ -0,0 +1,16 @@
+function crabs ()
+  % Draw the game map and initialize map dimensions.
+  [mapHeight , mapWidth] = drawMap( "BGImage.png" );
+  % Initialize captain location, heading and size
+  xCapt = 1000;
+  yCapt = 1200;
+  thetaCapt = -pi/2;
+  sizeCapt = 50;
+  % Draw the captain and initialize graphics handles
+  %*********************************************************
+  % Put your call to drawCapt() here ..... You must give drawCapt its
+  drawCapt (xCapt , yCapt , thetaCapt , sizeCapt)
+  % input and output arguments.
+  %*******************************************************
+endfunction
+
