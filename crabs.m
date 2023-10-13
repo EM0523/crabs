f555399a2288af1b00ba3640e0cdb65a34d9f942
EM0523 <145616364+EM0523@users.noreply.github.com>
2023-10-13 03:01:49 -0700

    crabs game character movement

--- crabs.m
+++ crabs.m
@@ -8,7 +8,29 @@ function crabs ()
   sizeCapt = 50;
   % Draw the captain and initialize graphics handles
   % Put your call to drawCapt() here ..... You must give drawCapt its
-  drawCapt (xCapt , yCapt , thetaCapt , sizeCapt)
+  captainGraphics = drawCapt(xCapt , yCapt , thetaCapt , sizeCapt)
   % input and output arguments.
-endfunction
 
+  cmd = "null"; % initial command
+
+  while ( cmd != "Q") % While not quit, read keyboard and respond
+
+  cmd = kbhit(); % Read the keyboard.
+
+  if( cmd == "w" || cmd == "a" || cmd == "d" ) %Captain has moved. Respond.
+
+  % erase old captain
+  for i=1:length( captGraphics )
+    set( captGraphics(i), 'Visible', 'off' );
+  endfor
+
+% move capt
+[xCapt, yCapt, thetaCapt] = moveCapt(cmd, xCapt, yCapt, thetaCapt);
+
+% draw new capt
+captGraphics = drawCapt( xCapt, yCapt, thetaCapt, sizeCapt);
+
+endif
+
+endwhile
+endfunction
